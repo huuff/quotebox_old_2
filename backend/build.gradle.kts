551a88d75bcb6f5c071f636b6e2a1bc769f1a48f
Haf <haf@protonmail.ch>
2022-10-02 11:26:19 +0200

    better to merge it all into a single project

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,24 +1,44 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10" apply false
+    kotlin("jvm") version "1.7.10"
+    id("org.openapi.generator") version "5.3.0"
 }
 
-allprojects {
-    group = "xyz.haff.quoteapi"
-    version = "0.1.0"
+group = "xyz.haff.quoteapi"
+version = "0.1.0"
 
-    repositories {
-        mavenCentral()
-    }
+repositories {
+    mavenCentral()
 }
 
-subprojects {
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+dependencies {
+    testImplementation(kotlin("test"))
 }
+
+val apiGeneratedSourcesDir = "$buildDir/generated-sources/api"
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$projectDir/../api/api.yaml") // TODO: From properties?
+    outputDir.set(apiGeneratedSourcesDir)
+    apiPackage.set("xyz.haff.quoteapi.controller")
+    modelPackage.set("xyz.haff.quoteapi.dto")
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "serializationLibrary" to "moshi", // TODO: not working?
+            "enumPropertyNaming" to "UPPERCASE",
+            "reactive" to "true",
+            "interfaceOnly" to "true",
+        )
+    )
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.compileKotlin {
+    kotlinOptions.jvmTarget = "17"
+    dependsOn(tasks.openApiGenerate)
+}
+
+sourceSets["main"].java.srcDir(file("$apiGeneratedSourcesDir/src/main/kotlin"))
