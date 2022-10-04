33a3567439f2c6d7e2b2f4090b2a6ff06bd7eefd
Haf <haf@protonmail.ch>
2022-10-04 17:15:10 +0200

    DataMongoTest

--- backend/src/test/kotlin/xyz/haff/quoteapi/data/repository/QuoteRepositoryTest.kt
+++ backend/src/test/kotlin/xyz/haff/quoteapi/data/repository/QuoteRepositoryTest.kt
@@ -3,20 +3,18 @@ package xyz.haff.quoteapi.data.repository
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import xyz.haff.quoteapi.data.entity.QuoteEntity
-import java.util.*
 
 
 // TODO: Hmmm why does it pick the SampleBean but not the QuoteRepository?
 @Testcontainers
-// TODO: Use DataMongoTest, maybe exclude embedded mongo
-@SpringBootTest
+@DataMongoTest
 class QuoteRepositoryTest : FunSpec() {
     @Autowired // TODO: try to add it from constructor
     private lateinit var quoteRepository: QuoteRepository
