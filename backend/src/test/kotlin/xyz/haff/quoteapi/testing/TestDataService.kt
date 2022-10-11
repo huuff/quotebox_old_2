04173db5b0022443718500e3e067998a6f82ebd3
Haf <haf@protonmail.ch>
2022-10-11 17:45:55 +0200

    more resilient test

--- backend/src/test/kotlin/xyz/haff/quoteapi/testing/TestDataService.kt
+++ backend/src/test/kotlin/xyz/haff/quoteapi/testing/TestDataService.kt
@@ -1,22 +1,23 @@
 package xyz.haff.quoteapi.testing
 
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactive.awaitLast
 import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Service
+import xyz.haff.quoteapi.data.entity.QuoteEntity
 import xyz.haff.quoteapi.data.repository.QuoteRepository
 
-// TODO: I should maybe test it without a repository because then if any repository-related config is wrong all tests
-// that depend on it will fail during initialization, which is harder to understand. (Maybe MongoTemplate?)
 @Service
 class TestDataService(
-    private val repository: QuoteRepository,
+    private val mongoTemplate: ReactiveMongoTemplate,
 ) {
     suspend fun load() {
-        repository.saveAll(TestData.entities).awaitLast()
+        mongoTemplate.insert(TestData.entities, QuoteEntity::class.java).awaitFirstOrNull()
     }
-
     suspend fun clear() {
-        repository.deleteAll().awaitSingleOrNull()
+        mongoTemplate.remove(Query(), QuoteEntity::class.java).awaitSingleOrNull()
     }
 }
 
