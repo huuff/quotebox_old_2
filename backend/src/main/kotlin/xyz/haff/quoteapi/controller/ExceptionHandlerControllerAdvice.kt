5d455095029534a0347e10c5e199b76d95d188d8
Haf <haf@protonmail.ch>
2022-10-24 16:37:21 +0200

    cleaned error handling code a bit

--- backend/src/main/kotlin/xyz/haff/quoteapi/controller/ExceptionHandlerControllerAdvice.kt
+++ backend/src/main/kotlin/xyz/haff/quoteapi/controller/ExceptionHandlerControllerAdvice.kt
@@ -1,11 +1,9 @@
 package xyz.haff.quoteapi.controller
 
 import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
-import org.hibernate.validator.internal.util.annotation.ConstraintAnnotationDescriptor
 import org.springframework.core.codec.DecodingException
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.beanvalidation.SpringValidatorAdapter
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
@@ -13,7 +11,6 @@ import org.springframework.web.bind.support.WebExchangeBindException
 import xyz.haff.quoteapi.dto.ValidationErrorDto
 import xyz.haff.quoteapi.util.createValidationError
 import javax.validation.ConstraintViolation
-import javax.validation.ConstraintViolationException
 import javax.validation.constraints.Size
 
 @ControllerAdvice
@@ -22,7 +19,7 @@ class ExceptionHandlerControllerAdvice {
 
     @ResponseBody
     @ExceptionHandler(DecodingException::class)
-    suspend fun decodingError(e: DecodingException): ResponseEntity<ValidationErrorDto> {
+    suspend fun decodingError(e: DecodingException): ResponseEntity<List<ValidationErrorDto>> {
         val result = when (val cause = e.cause) {
             is MissingKotlinParameterException -> createValidationError(
                 cause.parameter.name!!,
@@ -35,36 +32,36 @@ class ExceptionHandlerControllerAdvice {
         return ResponseEntity
             .badRequest()
             .contentType(MediaType.APPLICATION_JSON)
-            .body(result)
+            .body(listOf(result))
     }
 
-    // TODO: Do it better
+
     @ResponseBody
     @ExceptionHandler(WebExchangeBindException::class)
-    suspend fun bindException(e: WebExchangeBindException): ResponseEntity<ValidationErrorDto> {
-        val fieldError = e.bindingResult.fieldError
-        val constraintViolation = fieldError?.unwrap(ConstraintViolation::class.java)!!
-        val result = when (constraintViolation.constraintDescriptor.annotation) {
-            is Size -> when {
-                constraintViolation.constraintDescriptor.attributes["min"] != 0 -> createValidationError(
-                    path = constraintViolation.propertyPath.toString(),
-                    type = ValidationErrorDto.Type.MUST_BE_LONGER_THAN,
-                    parameter = constraintViolation.constraintDescriptor.attributes["min"]
-                )
+    suspend fun bindException(e: WebExchangeBindException): ResponseEntity<List<ValidationErrorDto>> {
+        val errors = e.bindingResult.fieldErrors.map { fieldError ->
+            val constraintViolation = fieldError?.unwrap(ConstraintViolation::class.java)!!
+
+            val path = constraintViolation.propertyPath.toString()
+            val descriptor = constraintViolation.constraintDescriptor
+            val annotation = descriptor.annotation
+            when {
+                annotation is Size && descriptor.attributes["min"] != 0 -> createValidationError(
+                        path = path,
+                        type = ValidationErrorDto.Type.MUST_BE_LONGER_THAN,
+                        parameter = descriptor.attributes["min"]
+                    )
                 else -> createValidationError(
-                    path = constraintViolation.propertyPath.toString(),
+                    path = path,
                     type = ValidationErrorDto.Type.UNKNOWN
                 )
             }
-            else -> createValidationError(
-                path = constraintViolation.propertyPath.toString(),
-                type = ValidationErrorDto.Type.UNKNOWN
-            )
         }
+
 
         return ResponseEntity
             .badRequest()
             .contentType(MediaType.APPLICATION_JSON)
-            .body(result)
+            .body(errors)
     }
 }
